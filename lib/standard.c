30a4d9c3040fcb9d9441339f47025c38d74980cd
endeav0r <endeavor@rainbowsandpwnies.com>
2011-08-28 22:05:27 -0400

    added standard lib function fread

--- lib/standard.c
+++ lib/standard.c
@@ -2,8 +2,9 @@
 
 void lib_standard_register (struct in_s * in)
 {
+    capi_register_function(in, lib_standard_fread, "fread");
     capi_register_function(in, lib_standard_print, "print");
-    capi_register_function(in, lib_standard_str, "str");
+    capi_register_function(in, lib_standard_str,   "str");
 }
 
 
@@ -48,4 +49,53 @@ int lib_standard_print (struct capi_s * capi)
     }
     
     return 0;
+}
+
+
+int lib_standard_fread (struct capi_s * capi)
+{
+    int filesize;
+    int bytes_read;
+    FILE * fh;
+    char * buf;
+    char * filename;
+    struct var_s * var;
+
+    if (capi_size(capi) != 1) {
+        fprintf(stderr, "LIB_STANDARD_FREAD: NUM ARGS != 1\n");
+        exit(-1);
+    }
+    
+    filename = capi_to_string(capi, 0);
+    
+    fh = fopen(filename, "rb");
+    if (fh == NULL) {
+        fprintf(stderr, "LIB_STANDARD_FREAD: FAILED OPENING %s\n", filename);
+        exit(-1);
+    }
+    
+    fseek(fh, 0, SEEK_END);
+    filesize = (int) ftell(fh);
+    fseek(fh, 0, SEEK_SET);
+    
+    buf = (char *) malloc(filesize + 1);
+    
+    
+    bytes_read = fread(buf, 1, filesize, fh);
+    if (bytes_read != filesize) {
+        fprintf(stderr, "LIB_STANDARD_FREAD: READ ERROR FILE %s, %d %d\n",
+                filename, filesize, bytes_read);
+        exit(-1);
+    }
+    
+    fclose(fh);
+    
+    buf[filesize] = (char) 0x00;
+    
+    var = var_create(TYPE_STRING, buf);
+    
+    capi_pop(capi);
+    capi_push(capi, var);
+    
+    return 1;
 }
--- lib/standard.h
+++ lib/standard.h
@@ -1,6 +1,8 @@
 #ifndef standard_HEADER
 #define standard_HEADER
 
+#include <stdio.h>
+
 #include "../src/interpreter.h"
 #include "../src/capi.h"
 
@@ -8,5 +10,7 @@ void lib_standard_register (struct in_s * in);
 
 int lib_standard_print (struct capi_s * capi);
 int lib_standard_str   (struct capi_s * capi);
+
+int lib_standard_fread (struct capi_s * capi);
 
 #endif
